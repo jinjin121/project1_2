cc9157a870c62eb7da803cd34560c9bb68eb85e5
jinjin121 <110872022+jinjin121@users.noreply.github.com>
2023-05-15 18:13:09 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+    var option = {
+        anchors: ['01', '02', '03', '04', '05'],
+        afterRender: function () {
+            setTimeout(function () { $('.section').eq(0).addClass('on') }, 10)
+        },
+        onLeave: function (index, nextIndex, direction) {
+            console.log(index, nextIndex, direction);
+            $('h1 span').text(nextIndex);
+            $('nav li').eq(nextIndex - 1).addClass('on')
+                .siblings().removeClass('on');
+            $('.section').eq(nextIndex - 1).addClass('on')
+                .siblings().removeClass('on');
+        },
+    }
+    $('.main').fullpage(option);
+
+})
